d7c4ab510cb1c996919072777896d4044a1b9076
Betelhem-hailu <betelhemhailu811@gmail.com>
2024-07-25 14:09:15 +0300

    fix:mongo connection

--- song_app_server/src/server.js
+++ song_app_server/src/server.js
@@ -3,6 +3,20 @@ const songRoutes = require('./routes/songRoutes.js');
 
 const port = process.env.PORT || 8000;
 
+module.exports = async (arg1, arg2, arg3) => {
+
+  await app().then(async mongoose => {
+      try{
+          console.log('Connected to mongo!!');
+          await command.execute(client, message, args);
+      }
+      finally{
+          mongoose.connection.close();
+      }
+  });
+
+};
+
 app.get("/", (req, res) => {
     res.send("Here is Backend...");
   });
